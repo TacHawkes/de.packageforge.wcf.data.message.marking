52f8c094d5f1047225b26e1d34f6006a14323f10
Oliver Kliebisch <oliver@kliebisch.net>
2011-09-29 14:35:16 +0200

    -install script added

---
+++ files/acp/markteamInstall.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Activates group permissions for admins
+ *
+ * @author      Oliver Kliebisch
+ * @copyright   2011 Oliver Kliebisch
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package     de.packageforge.wcf.markteam
+ * @subpackage  acp
+ * @category    Community Framework
+ */
+// admin options
+$sql = "UPDATE 	wcf".WCF_N."_group_option_value
+	SET	optionValue = 1
+	WHERE	groupID = 4
+	AND     optionID IN     (
+				SELECT	optionID
+				FROM    wcf".WCF_N."_group_option
+				WHERE	packageID = ".$this->installation->getPackageID()."
+				)
+                AND     optionValue = '0'";
+WCF::getDB()->sendQuery($sql);
+?>
